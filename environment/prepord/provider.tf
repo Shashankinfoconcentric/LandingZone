c9d9019278df238717c498621ce876e32c3a0580
Your Name <you@example.com>
2026-08-03 05:20:30 +0530

    my first commit

---
+++ environment/prepord/provider.tf
@@ -0,0 +1,15 @@
+stterraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.81.0"
+    }
+  }
+}
+provider "azurerm" {
+features {
+resource_group {
+prevent_deletion_if_contains_resources = false
+}
+}
+}
---
+++ environment/prepord/variable.tf
@@ -0,0 +1,5 @@
+variable "rgsmod" {}
+variable "virtual_network" {}
+variable "subnet" {}
+variable "public_ip" {}
+variable "vm" {}
